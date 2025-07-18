b72f3161ba421f2bf33b5e547d6ee38b60657852
samsara <147016591+tx-samsara@users.noreply.github.com>
2025-07-18 11:07:35 +0800

    Update PokeVersionUtils.java
    init

--- src/main/java/com/create/creativeCore/PixelmonApi/PokeVersionUtils.java
+++ src/main/java/com/create/creativeCore/PixelmonApi/PokeVersionUtils.java
@@ -1,10 +1,1 @@
-package com.create.creativeCore.PixelmonApi;
 
-import com.pixelmonmod.pixelmon.api.storage.PCStorage;
-import com.pixelmonmod.pixelmon.storage.PlayerPartyStorage;
-import org.bukkit.entity.Player;
-
-public interface PokeVersionUtils {
-    public PlayerPartyStorage getParty(Player player);
-    public PCStorage getPC(Player player);
-}
