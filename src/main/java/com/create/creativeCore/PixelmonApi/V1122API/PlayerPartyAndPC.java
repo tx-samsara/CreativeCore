3e45c5bb7d49b98a9e84cf60b51046cc4253d235
samsara <147016591+tx-samsara@users.noreply.github.com>
2025-07-18 11:10:01 +0800

    Update PlayerPartyAndPC.java
    i

--- src/main/java/com/create/creativeCore/PixelmonApi/V1122API/PlayerPartyAndPC.java
+++ src/main/java/com/create/creativeCore/PixelmonApi/V1122API/PlayerPartyAndPC.java
@@ -1,18 +1,15 @@
 package com.create.creativeCore.PixelmonApi.V1122API;
 
-import com.create.creativeCore.PixelmonApi.PokeVersionUtils;
 import com.pixelmonmod.pixelmon.Pixelmon;
 import com.pixelmonmod.pixelmon.api.storage.PCStorage;
 import com.pixelmonmod.pixelmon.storage.PlayerPartyStorage;
 import org.bukkit.entity.Player;
 
-public class PlayerPartyAndPC implements PokeVersionUtils {
-    @Override
-    public PlayerPartyStorage getParty(Player player){
+public class PlayerPartyAndPC {
+    public static PlayerPartyStorage getParty(Player player){
         return Pixelmon.storageManager.getParty(player.getUniqueId());
     }
-    @Override
-    public PCStorage getPC(Player player){
+    public static PCStorage getPC(Player player){
         return Pixelmon.storageManager.getPCForPlayer(player.getUniqueId());
     }
 }
